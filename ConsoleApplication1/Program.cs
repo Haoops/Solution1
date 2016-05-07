99fa88f929d7f5d23a560fbdedd0076712481398
haoops <haoops1217@gmail.com>
2016-05-07 11:02:12 +0800

    Initial.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedProject1;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+
+            p.id = 1;
+            p.name = "Kevin";
+
+            Console.WriteLine(p.name);
+
+        }
+    }
+}
---
+++ SharedProject1/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
